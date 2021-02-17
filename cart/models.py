38c7fc23aed3abc8b4cd01e48f656593b1480474
markkimjr <markkimjr96@gmail.com>
2021-02-17 21:49:42 +0900

    MODIFY : on_delete values

--- cart/models.py
+++ cart/models.py
@@ -6,8 +6,8 @@ from user.models    import User
 
 
 class Cart(models.Model):
-    product = models.ForeignKey('product.Product', on_delete=models.SET_NULL, null=True, related_name='carts')
-    voucher = models.ForeignKey('voucher.Voucher', on_delete=models.SET_NULL, null=True, related_name='carts')
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, related_name='carts')
+    voucher = models.ForeignKey('voucher.Voucher', on_delete=models.CASCADE, related_name='carts')
 
     class Meta:
         db_table = 'carts'
--- product/models.py
+++ product/models.py
@@ -9,7 +9,7 @@ class BagType(models.Model):
 class BagModel(models.Model):
     model_name     = models.CharField(max_length=100)
     video          = models.URLField()
-    bag_type       = models.ForeignKey('BagType', on_delete=models.SET_NULL, null=True, related_name='bag_models')
+    bag_type       = models.ForeignKey('BagType', on_delete=models.CASCADE, related_name='bag_models')
 
     class Meta:
         db_table = 'bag_models'
@@ -18,33 +18,33 @@ class Product(models.Model):
     model_number = models.CharField(max_length=100)
     price        = models.DecimalField(max_digits=15, decimal_places=2)
     description  = models.TextField()
-    bag_model    = models.ForeignKey('BagModel', on_delete=models.SET_NULL, null=True, related_name='products')
+    bag_model    = models.ForeignKey('BagModel', on_delete=models.CASCADE, related_name='products')
+    color        = models.ForeignKey('Color', on_delete=models.CASCADE, related_name='products')
+    size         = models.ForeignKey('Size', on_delete=models.CASCADE, related_name='products')
 
     class Meta:
         db_table = 'products'
 
 class Color(models.Model):
     color_name    = models.CharField(max_length=100)
-    product       = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='colors')
 
     class Meta:
         db_table = 'colors'
 
 class Size(models.Model):
     size_name    = models.CharField(max_length=100)
-    product      = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='sizes')
 
     class Meta:
         db_table = 'sizes'
 
 class Image(models.Model):
     image_url = models.URLField()
-    product   = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='images')
+    product   = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='images')
 
     class Meta:
         db_table = 'images'
 
 class Recommendation(models.Model):
-    product   = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='recommendations')
-    bag_model = models.ForeignKey('BagModel', on_delete=models.SET_NULL, null=True, related_name='recommendations')
+    product   = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='recommendations')
+    bag_model = models.ForeignKey('BagModel', on_delete=models.CASCADE, related_name='recommendations')
 
