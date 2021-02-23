44b4976161050f1fa9cfdd38add36025ee0efb82
banana1019 <jyho930@gmail.com>
2021-02-24 05:37:37 +0900

    Modify: 멘토 리뷰 반영, OrderDetailView 클래스 get 메소드 추가

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from user.views  import OrderDetailView
+from cart.views  import OrderDetailView
 
 urlpatterns = [
     path('/checkout', OrderDetailView.as_view()),
--- cart/views.py
+++ cart/views.py
@@ -8,59 +8,64 @@ from cart.models    import Cart, Order, OrderStatus, AddressInformation
 from user.models    import User
 from product.models import Product
 from voucher.models import Voucher
+from user.utils     import login_decorator
 
 class OrderDetailView(View):
     def post(self, request):
         try:
-            data                        = json.loads(request.body)
-            email                       = data['email']
-            billing_country             = data['billing_country']
-            billing_first_name          = data['billing_first_name']
-            billing_last_name           = data['billing_last_name']
-            billing_street_number       = data['billing_street_number']
-            billing_additional_address  = data['billing_additional_address']
-            billing_district            = data['billing_district']
-            billing_city                = data['billing_city']
-            billing_postal_code         = data['billing_postal_code']
-            billing_phone_number        = data['billing_phone_number']
-            shipping_country            = data['shipping_country']
-            shipping_first_name         = data['shipping_first_name']
-            shipping_last_name          = data['shipping_last_name']
-            shipping_street_number      = data['shipping_street_number']
-            shipping_additional_address = data['shipping_additional_address']
-            shipping_district           = data['shipping_district']
-            shipping_city               = data['shipping_city']
-            shipping_postal_code        = data['shipping_postal_code']
-            shipping_phone_number       = data['shipping_phone_number']
+            data = json.loads(request.body)
             
-            if User.objects.filter(email=email).exists():
-                user = User.objects.get(email=email)
+            if User.objects.filter(email=data['email']).exists():
+                user = User.objects.get(email=data['email'])
             else:
-                user = User.objects.create(email=email, is_anonymous=True)
+                user = User.objects.create(email=data['email'], is_anonymous=True)
             
             AddressInformation.objects.create(
-                user_id=user.id,
-                billing_country=billing_country,
-                billing_first_name=billing_first_name,
-                billing_last_name=billing_last_name,
-                billing_street_number=billing_street_number,
-                billing_additional_address=billing_additional_address,
-                billing_district=billing_district,
-                billing_city=billing_city,
-                billing_postal_code=billing_postal_code,
-                billing_phone_number=billing_phone_number,
-                shipping_country=shipping_country,
-                shipping_first_name=shipping_first_name,
-                shipping_last_name=shipping_last_name,
-                shipping_street_number=shipping_street_number,
-                shipping_additional_address=shipping_additional_address,
-                shipping_district=shipping_district,
-                shipping_city=shipping_city,
-                shipping_postal_code=shipping_postal_code,
-                shipping_phone_number=shipping_phone_number
+                user_id                     = user.id,
+                billing_country             = data['billing_country']
+                billing_first_name          = data['billing_first_name']
+                billing_last_name           = data['billing_last_name']
+                billing_street_number       = data['billing_street_number']
+                billing_additional_address  = data['billing_additional_address']
+                billing_district            = data['billing_district']
+                billing_city                = data['billing_city']
+                billing_postal_code         = data['billing_postal_code']
+                billing_phone_number        = data['billing_phone_number']
+                shipping_country            = data['shipping_country']
+                shipping_first_name         = data['shipping_first_name']
+                shipping_last_name          = data['shipping_last_name']
+                shipping_street_number      = data['shipping_street_number']
+                shipping_additional_address = data['shipping_additional_address']
+                shipping_district           = data['shipping_district']
+                shipping_city               = data['shipping_city']
+                shipping_postal_code        = data['shipping_postal_code']
+                shipping_phone_number       = data['shipping_phone_number']
             )
             
             return JsonResponse({'message': 'SUCCESS'}, status=200)
         
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+    @login_decorator
+    def get(self, request):
+        user = request.user
+        order_detail = {
+            'email'                  : user.email,
+            'shipping_first_name'    : user.address_information.get(user=user).shipping_first_name,
+            'shipping_last_name'     : user.address_information.get(user=user).shipping_last_name,
+            'shipping_street_number' : user.address_information.get(user=user).shipping_street_number,
+            'shipping_district'      : user.address_information.get(user=user).shipping_district,
+            'shipping_city'          : user.address_information.get(user=user).shipping_city,
+            'shipping_postal_code'   : user.address_information.get(user=user).shipping_postal_code,
+            'shipping_country'       : user.address_information.get(user=user).shipping_country,
+            'billing_first_name'     : user.address_information.get(user=user).billing_first_name,
+            'billing_last_name'      : user.address_information.get(user=user).billing_last_name,
+            'billing_street_number'  : user.address_information.get(user=user).billing_street_number,
+            'billing_district'       : user.address_information.get(user=user).billing_district,
+            'billing_city'           : user.address_information.get(user=user).billing_city,
+            'billing_postal_code'    : user.address_information.get(user=user).billing_postal_code,
+            'billing_country'        : user.address_information.get(user=user).billing_country,
+            'billing_phone_number'   : user.address_information.get(user=user).billing_phone_number,
+        }
+        return JsonResponse({'message': 'SUCCESS', 'order_detail': order_detail}, status=200)
