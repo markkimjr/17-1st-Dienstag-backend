f0d092491faca6ed0e5ee4b25717d911ab97126e
banana1019 <jyho930@gmail.com>
2021-02-23 16:47:45 +0900

    Modify: 청구 정보, 배송 정보 입력 - cart/modesl.py: is_same 컬럼 추가 - user/models.py: is_anonymous 컬럼에 default 값 추가 - user/views.py: UserView 클래스 추가

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from user.views  import SignUpView, SignInView, NonMemberView
+from user.views  import SignUpView, SignInView, UserView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
-    path('/checkout', NonMemberView.as_view()),
+    path('/checkout', UserView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -14,27 +14,26 @@ class SignUpView(View):
     def post(self, request):
         MINIMUM_PASSWORD_LENGTH = 8
         try:
-            data         = json.loads(request.body)
-            email        = data['email']
-            is_anonymous = data['is_anonymous']
+            data            = json.loads(request.body)
+            email           = data['email']
+            username        = data['username']
+            password        = data['password']
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            phone_number    = data['phone_number']
+
             if User.objects.filter(email=email).exists():
                 return JsonResponse({'message': 'ALREADY_EXIST'}, status=400)
 
             if '@' not in email or '.' not in email:
                 return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
-            
+
+            if len(password) < MINIMUM_PASSWORD_LENGTH:
+                return JsonResponse({'message': 'PASSWORD_VALIDATION_ERROR'}, status=400)
+                
             user = User.objects.create(email=email, is_anonymous=False)
             
-            if not user.is_anonymous:
-                username        = data['username']
-                password        = data['password']
-                hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-                phone_number    = data['phone_number']
-
-                if len(password) < MINIMUM_PASSWORD_LENGTH:
-                    return JsonResponse({'message': 'PASSWORD_VALIDATION_ERROR'}, status=400)
-                    
-                UserInformation.objects.create(user=user, username=username, password=hashed_password, phone_number=phone_number)
+            UserInformation.objects.create(user=user, username=username, password=hashed_password, phone_number=phone_number)
+            
             return JsonResponse({'message': 'SUCCESS'}, status=200)
 
         except KeyError:
@@ -60,17 +59,82 @@ class SignInView(View):
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-class NonMemberView(View):
+class UserView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
-            email = data['email']
-
+            data                        = json.loads(request.body)
+            email                       = data['email']
+            billing_country             = data['billing_country']
+            billing_first_name          = data['billing_first_name']
+            billing_last_name           = data['billing_last_name']
+            billing_street_number       = data['billing_street_number']
+            billing_additional_address  = data['billing_additional_address']
+            billing_district            = data['billing_district']
+            billing_city                = data['billing_city']
+            billing_postal_code         = data['billing_postal_code']
+            billing_phone_number        = data['billing_phone_number']
+            is_same                     = data['is_same']
+            
             if User.objects.filter(email=email).exists():
-                return JsonResponse({'message': 'ALREADY_EXIST'}, status=400)
+                user = User.objects.get(email=email)
+            else:
+                user = User.objects.create(email=email, is_anonymous=True)
 
-            if not User.objects.filter(email=email).exists():
-                User.objects.create(email=email, is_anonymous=True)
+            if is_same == 'True':
+                AddressInformation.objects.create(
+                    user_id=user.id,
+                    billing_country=billing_country,
+                    billing_first_name=billing_first_name,
+                    billing_last_name=billing_last_name,
+                    billing_street_number=billing_street_number,
+                    billing_additional_address=billing_additional_address,
+                    billing_district=billing_district,
+                    billing_city=billing_city,
+                    billing_postal_code=billing_postal_code,
+                    billing_phone_number=billing_phone_number,
+                    shipping_country=billing_country,
+                    shipping_first_name=billing_first_name,
+                    shipping_last_name=billing_last_name,
+                    shipping_street_number=billing_street_number,
+                    shipping_additional_address=billing_additional_address,
+                    shipping_district=billing_district,
+                    shipping_city=billing_city,
+                    shipping_postal_code=billing_postal_code,
+                    shipping_phone_number=billing_phone_number
+            )
+
+            if is_same == 'False':
+                shipping_country            = data['shipping_country']
+                shipping_first_name         = data['shipping_first_name']
+                shipping_last_name          = data['shipping_last_name']
+                shipping_street_number      = data['shipping_street_number']
+                shipping_additional_address = data['shipping_additional_address']
+                shipping_district           = data['shipping_district']
+                shipping_city               = data['shipping_city']
+                shipping_postal_code        = data['shipping_postal_code']
+                shipping_phone_number       = data['shipping_phone_number']
+                AddressInformation.objects.create(
+                    user_id=user.id,
+                    billing_country=billing_country,
+                    billing_first_name=billing_first_name,
+                    billing_last_name=billing_last_name,
+                    billing_street_number=billing_street_number,
+                    billing_additional_address=billing_additional_address,
+                    billing_district=billing_district,
+                    billing_city=billing_city,
+                    billing_postal_code=billing_postal_code,
+                    billing_phone_number=billing_phone_number,
+                    shipping_country=shipping_country,
+                    shipping_first_name=shipping_first_name,
+                    shipping_last_name=shipping_last_name,
+                    shipping_street_number=shipping_street_number,
+                    shipping_additional_address=shipping_additional_address,
+                    shipping_district=shipping_district,
+                    shipping_city=shipping_city,
+                    shipping_postal_code=shipping_postal_code,
+                    shipping_phone_number=shipping_phone_number
+            )
+            
             return JsonResponse({'message': 'SUCCESS'}, status=200)
 
         except KeyError:
