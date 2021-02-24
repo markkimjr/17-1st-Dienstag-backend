7060e74187c7f9e46d7a5846152dd2cfd11968c1
markkimjr <markkimjr96@gmail.com>
2021-02-24 17:32:18 +0900

    MODIFY : cart/urls.py (added cart_id to url path)

--- cart/urls.py
+++ cart/urls.py
@@ -4,7 +4,7 @@ from cart.views  import CartView, CartProductView, CartVoucherView
 
 urlpatterns = [
         path('', CartView.as_view()),
-        path('/<int:item_id>', CartView.as_view()),
+        path('/<int:cart_id>', CartView.as_view()),
         path('/product', CartProductView.as_view()),
         path('/voucher', CartVoucherView.as_view()),
         ]
