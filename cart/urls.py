af2c5cc96439c6d7b88e3b702af9d61afb17e597
markkimjr <markkimjr96@gmail.com>
2021-02-26 09:54:06 +0900

    MODIFY : added NonUserCartView

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from cart.views  import CartView
+from cart.views  import CartView, NonUserCartView
 
 urlpatterns = [
         path('', CartView.as_view()),
         path('/<int:cart_id>', CartView.as_view()),
+        path('/nonuser', NonUserCartView.as_view()),
+        path('/nonuser/<int:product_id>', NonUserCartView.as_view())
         ]
 
--- cart/views.py
+++ cart/views.py
@@ -89,3 +89,88 @@ class CartView(View):
         except Cart.DoesNotExist:
             return JsonResponse({'message': 'INVALID_CART'}, status=400)
 
+class NonUserCartView(View):
+    def get(self, request, product_id):
+        try:
+            product      = Product.objects.get(id=product_id)
+            product_info = [{
+                'item_id'      : product_id,
+                'model_number' : product.model_number,
+                'title'        : product.bag_model.name,
+                'price'        : product.price,
+                'quantity'     : 1,
+                'image_url'    : product.images.all()[0].image_url
+                }]
+            
+            return JsonResponse({'data': product_info}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+        except Product.DoesNotExist:
+            return JsonResponse({'message': 'INVALID_PRODUCT'}, status=400)
+
+    def post(self, request):
+        try:
+            data                        = json.loads(request.body)
+            product_list                = data['product_list']
+            email                       = data['email']
+            shipping_first_name         = data['shipping_first_name']
+            shipping_last_name          = data['shipping_last_name']
+            shipping_district           = data['shipping_district']
+            shipping_city               = data['shipping_city']
+            shipping_postal_code        = data['shipping_postal_code']
+            shipping_phone_number       = data['shipping_phone_number']
+            shipping_street_number      = data['shipping_street_number']
+            shipping_product_list       = data['shipping_product_list']
+            shipping_additional_address = data.get('shipping_additional_address')
+            billing_first_name          = data['billing_first_name']
+            billing_last_name           = data['billing_last_name']
+            billing_district            = data['billing_district']
+            billing_city                = data['billing_city']
+            billing_postal_code         = data['billing_postal_code']
+            billing_phone_number        = data['billing_phone_number']
+            billing_street_number       = data['billing_street_number']
+            billing_product_list        = data['billing_product_list']
+            billing_additional_address  = data.get('billing_additional_address')
+
+            user = User.objects.create(
+                    email        = email,
+                    is_anonymous = True
+                    )
+            
+            address = AddressInformation.objects.create(
+                    shipping_first_name         = shipping_first_name,
+                    shipping_last_name          = shipping_first_name,
+                    shipping_district           = shipping_district,
+                    shipping_city               = shipping_city,
+                    shipping_postal_code        = shipping_postal_code,
+                    shipping_phone_number       = shipping_phone_number,
+                    shipping_street_number      = shipping_street_number,
+                    shipping_additional_address = shipping_additional_address,
+                    billing_first_name          = billing_first_name,
+                    billing_last_name           = billing_first_name,
+                    billing_district            = billing_district,
+                    billing_city                = billing_city,
+                    billing_postal_code         = billing_postal_code,
+                    billing_phone_number        = billing_phone_number,
+                    billing_street_number       = billing_street_number,
+                    billing_additional_address  = billing_additional_address
+                    )
+
+            order = Order.objects.create(
+                    user_id                = user.id,
+                    order_status_id        = OrderStatus.objects.get(name=ORDER_STATUS_CHECK).id,
+                    address_information_id = address.id
+                    )
+
+            for i in range(product_list):
+                Cart.objects.create(
+                product_id = product_list[i],
+                order_id = order.id 
+                )
+
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
