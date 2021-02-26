7035e2b2a83e0d3354431cc01b71a1974636f9e2
banana1019 <jyho930@gmail.com>
2021-02-26 11:38:04 +0900

    ADD: OrderDetailView

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from cart.views  import OrderDetailView
 
 urlpatterns = [
+    path('', CartView.as_view()),
+    path('/<int:cart_id>', CartView.as_view()),
     path('/checkout', OrderDetailView.as_view()),
 ]
